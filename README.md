16e8dcf3ec3383ae782a28c448981752324e7927
Benmrad Bilel <109316958+BenMradB@users.noreply.github.com>
2024-06-14 15:55:25 +0100

    First Commit of the extension

---
+++ README.md
@@ -0,0 +1,106 @@
+# VSCode Clean Code Extension
+
+This Visual Studio Code (VSCode) extension provides tools to help you maintain clean and readable code by automating the removal of comments, console logs, and unused imports. It includes individual commands for each of these tasks and a comprehensive command that combines all of them.
+
+### Commands
+
+1. **Remove Comments**
+
+   - **Command:** `Remove Comments`
+   - **Description:** Removes all single-line (`//`) and multi-line (`/* */`) comments from the active file, making your code more concise and easier to read.
+
+2. **Remove Console Logs**
+
+   - **Command:** `Remove Console Logs`
+   - **Description:** Removes all `console.log` statements from the active file, ensuring that no debug logs are left in your production code.
+
+3. **Remove Unused Imports**
+
+   - **Command:** `Remove Unused Imports`
+   - **Description:** Analyzes the active file to identify and remove any unused import statements, keeping your codebase clean and efficient.
+
+4. **Clean Code**
+   - **Command:** `Clean Code`
+   - **Description:** Combines the functionality of removing comments, console logs, and unused imports into a single command, providing a one-stop solution for cleaning up your code.
+
+## Usage
+
+1. Open a JavaScript or TypeScript file in VSCode.
+2. Run your command from the command palette by pressing (`Ctrl+Shift+P` or `Cmd+Shift+P` on Mac).
+3. Type one of the following commands and press Enter:
+   - `Remove Comments`
+   - `Remove Console Logs`
+   - `Remove Unused Imports`
+   - `Clean Code`
+
+## Example
+
+Here is an example JavaScript file before and after using the extension:
+
+**Before 👎🤦‍♂️**
+
+```typescript
+import Image from "next/image";
+import Link from "next/link";
+import { useState, useRef, useEffect, useReducer, useContext } from "react";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+
+/* import {
+  CalendarIcon,
+  EnvelopeClosedIcon,
+  FaceIcon,
+  GearIcon,
+} from "@radix-ui/react-icons"; */
+
+export default function HomePage() {
+  const [state, setState] = useState(0);
+
+  console.log(
+    "This is a console log will be removed by the Clean Code extension."
+  );
+  useEffect(() => {}, []);
+  console.log(
+    "This is a console log will be removed by the Clean Code extension."
+  );
+
+  return (
+    <main className="w-full h-full">
+      This is a Next.js project that uses `Clean Code` extension.
+      <Image src="/logo.png" alt="logo" width={200} height={200} />
+      <Button></Button>
+      {/* <ul>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+      </ul> */}
+    </main>
+  );
+}
+```
+
+**After ✨🙆‍♂️**
+
+```typescript
+import Image from "next/image";
+
+import { useState, useEffect } from "react";
+import { Button } from "@/components/ui/button";
+
+export default function HomePage() {
+  const [state, setState] = useState(0);
+
+  useEffect(() => {}, []);
+
+  return (
+    <main className="w-full h-full">
+      This is a Next.js project that uses `Clean Code` extension.
+      <Image src="/logo.png" alt="logo" width={200} height={200} />
+      <Button></Button>
+    </main>
+  );
+}
+```
---
+++ src/extension.ts
@@ -0,0 +1,329 @@
+// The module 'vscode' contains the VS Code extensibility API
+// Import the module and reference it with the alias vscode in your code below
+import * as vscode from "vscode";
+import * as ts from "typescript";
+
+// This method is called when your extension is activated
+// Your extension is activated the very first time the command is executed
+export function activate(context: vscode.ExtensionContext) {
+  const removeCommentsHandler = () => {
+    const editor = vscode.window.activeTextEditor;
+
+    if (!editor) {
+      return vscode.window.showErrorMessage("No active file found.");
+    }
+    const document = editor.document;
+    const text = document.getText();
+
+    let inCommentBlock = false;
+    const cleanedText = text
+      .split("\n")
+      .filter((line) => {
+        const trimmedLine = line.trim();
+        if (trimmedLine.startsWith("/*") || trimmedLine.startsWith("{/*")) {
+          inCommentBlock = true;
+        }
+        if (trimmedLine.endsWith("*/") || trimmedLine.endsWith("*/}")) {
+          inCommentBlock = false;
+          return false;
+        }
+        return !inCommentBlock && !trimmedLine.startsWith("//");
+      })
+      .join("\n");
+
+    editor.edit((editBuilder) => {
+      editBuilder.replace(
+        new vscode.Range(
+          document.positionAt(0),
+          document.positionAt(text.length)
+        ),
+        cleanedText
+      );
+    });
+  };
+
+  const removeConsolLogsHandler = () => {
+    const editor = vscode.window.activeTextEditor;
+
+    if (!editor) {
+      return vscode.window.showErrorMessage("No active file found.");
+    }
+    const document = editor.document;
+    const text = document.getText();
+
+    const consoleLogRegex = /console\.log\(([\s\S]*?)\);?/g;
+    const cleanedText = text.replace(consoleLogRegex, "");
+
+    editor.edit((editBuilder) => {
+      editBuilder.replace(
+        new vscode.Range(
+          document.positionAt(0),
+          document.positionAt(text.length)
+        ),
+        cleanedText
+      );
+    });
+  };
+
+  const removeUnusedImportsHandler = () => {
+    const editor = vscode.window.activeTextEditor;
+
+    if (!editor) {
+      return vscode.window.showErrorMessage("No active file found.");
+    }
+    const document = editor.document;
+    const text = document.getText();
+
+    const sourceFile = ts.createSourceFile(
+      document.fileName,
+      text,
+      ts.ScriptTarget.ESNext,
+      true
+    );
+
+    const imports: { name: string; isDefault: boolean; many: boolean }[] = [];
+    const usedIdentifiers: string[] = [];
+
+    ts.forEachChild(sourceFile, function visit(node) {
+      if (ts.isImportDeclaration(node)) {
+        const importClause = node.importClause;
+        if (importClause) {
+          if (
+            importClause.name &&
+            (!importClause.name.text.startsWith("//") ||
+              !importClause.name.text.startsWith("/*"))
+          ) {
+            imports.push({
+              name: importClause.name.text,
+              isDefault: true,
+              many: false,
+            });
+          } else if (
+            importClause.namedBindings &&
+            ts.isNamedImports(importClause.namedBindings)
+          ) {
+            const elements: ts.NodeArray<ts.ImportSpecifier> =
+              importClause.namedBindings.elements;
+            elements.forEach((element) => {
+              if (
+                !element.name.text.startsWith("//") ||
+                !element.name.text.startsWith("/*")
+              ) {
+                imports.push({
+                  name: element.name.text,
+                  isDefault: false,
+                  many: elements.length > 1,
+                });
+              }
+            });
+          }
+        }
+      } else if (ts.isIdentifier(node) && node.parent.kind) {
+        usedIdentifiers.push(node.text);
+      }
+      ts.forEachChild(node, visit);
+    });
+
+    console.log("Imports:", imports);
+
+    const unusedImports = imports.filter(
+      (imp) =>
+        usedIdentifiers.filter((identifier) => identifier === imp.name).length <
+        2
+    );
+
+    console.log("Unused Imports:", unusedImports);
+
+    const cleanedText = text
+      .split("\n")
+      .map((line) => {
+        let modifiedLine = line;
+
+        unusedImports.forEach((unusedImport) => {
+          if (line.includes(unusedImport.name)) {
+            if (unusedImport.isDefault) {
+              modifiedLine = "";
+            } else {
+              if (unusedImport.many) {
+                modifiedLine = modifiedLine.replace(
+                  new RegExp(`, ${unusedImport.name}`, "g"),
+                  ""
+                );
+              } else {
+                modifiedLine = "";
+              }
+            }
+          }
+        });
+
+        return modifiedLine;
+      })
+      .join("\n");
+
+    editor.edit((editBuilder) => {
+      editBuilder.replace(
+        new vscode.Range(
+          document.positionAt(0),
+          document.positionAt(text.length)
+        ),
+        cleanedText
+      );
+    });
+  };
+
+  const cleanCodeHandler = () => {
+    const editor = vscode.window.activeTextEditor;
+
+    if (!editor) {
+      return vscode.window.showErrorMessage("No active file found.");
+    }
+
+    const document = editor.document;
+    const text = document.getText();
+
+    let inCommentBlock = false;
+    const cleanedTextWithoutComments = text
+      .split("\n")
+      .filter((line) => {
+        const trimmedLine = line.trim();
+        if (trimmedLine.startsWith("/*") || trimmedLine.startsWith("{/*")) {
+          inCommentBlock = true;
+        }
+        if (trimmedLine.endsWith("*/") || trimmedLine.endsWith("*/}")) {
+          inCommentBlock = false;
+          return false;
+        }
+        return !inCommentBlock && !trimmedLine.startsWith("//");
+      })
+      .join("\n");
+
+    const consoleLogRegex = /console\.log\([^)]*\);?/g;
+    const cleanedTextWithoutConsoleLogs = cleanedTextWithoutComments.replace(
+      consoleLogRegex,
+      ""
+    );
+
+    const sourceFile = ts.createSourceFile(
+      document.fileName,
+      cleanedTextWithoutConsoleLogs,
+      ts.ScriptTarget.ESNext,
+      true
+    );
+
+    const imports: { name: string; isDefault: boolean; many: boolean }[] = [];
+    const usedIdentifiers: string[] = [];
+
+    ts.forEachChild(sourceFile, function visit(node) {
+      if (ts.isImportDeclaration(node)) {
+        const importClause = node.importClause;
+        if (importClause) {
+          if (
+            importClause.name &&
+            (!importClause.name.text.startsWith("//") ||
+              !importClause.name.text.startsWith("/*"))
+          ) {
+            imports.push({
+              name: importClause.name.text,
+              isDefault: true,
+              many: false,
+            });
+          } else if (
+            importClause.namedBindings &&
+            ts.isNamedImports(importClause.namedBindings)
+          ) {
+            const elements: ts.NodeArray<ts.ImportSpecifier> =
+              importClause.namedBindings.elements;
+            elements.forEach((element) => {
+              if (
+                !element.name.text.startsWith("//") ||
+                !element.name.text.startsWith("/*")
+              ) {
+                imports.push({
+                  name: element.name.text,
+                  isDefault: false,
+                  many: elements.length > 1,
+                });
+              }
+            });
+          }
+        }
+      } else if (ts.isIdentifier(node) && node.parent.kind) {
+        usedIdentifiers.push(node.text);
+      }
+      ts.forEachChild(node, visit);
+    });
+
+    const unusedImports = imports.filter(
+      (imp) =>
+        usedIdentifiers.filter((identifier) => identifier === imp.name).length <
+        2
+    );
+
+    console.log("Unused Imports:", unusedImports);
+
+    const cleanedText = cleanedTextWithoutConsoleLogs
+      .split("\n")
+      .map((line) => {
+        let modifiedLine = line;
+        unusedImports.forEach((unusedImport) => {
+          if (line.includes(unusedImport.name)) {
+            if (unusedImport.isDefault) {
+              modifiedLine = "";
+            } else {
+              if (unusedImport.many) {
+                modifiedLine = modifiedLine.replace(
+                  new RegExp(`, ${unusedImport.name}`, "g"),
+                  ""
+                );
+              } else {
+                modifiedLine = "";
+              }
+            }
+          }
+        });
+
+        return modifiedLine;
+      })
+      .join("\n");
+
+    editor.edit((editBuilder) => {
+      editBuilder.replace(
+        new vscode.Range(
+          document.positionAt(0),
+          document.positionAt(text.length)
+        ),
+        cleanedText
+      );
+    });
+  };
+
+  const removeUnusedImports = vscode.commands.registerCommand(
+    "clean-code.removeUnusedImports",
+    removeUnusedImportsHandler
+  );
+
+  const removeComments = vscode.commands.registerCommand(
+    "clean-code.removeComments",
+    removeCommentsHandler
+  );
+
+  const removeConsoleLogs = vscode.commands.registerCommand(
+    "clean-code.removeConsoleLogs",
+    removeConsolLogsHandler
+  );
+
+  const cleanCode = vscode.commands.registerCommand(
+    "clean-code.cleanCode",
+    cleanCodeHandler
+  );
+
+  context.subscriptions.push(
+    removeComments,
+    removeConsoleLogs,
+    removeUnusedImports,
+    cleanCode
+  );
+}
+
+// This method is called when your extension is deactivated
+export function deactivate() {}
